142094b3e5bbbe437426a225b88f4b28b6e51149
Miguel Peixe <miguel@cardume.art.br>
2016-04-04 11:11:42 -0300

    fix ga

--- public/js/app.js
+++ public/js/app.js
@@ -86,6 +86,7 @@ angular.module('rastrodelama', [
   function($rootScope, $location, $window) {
     $rootScope.$on('$stateChangeSuccess', function(ev, toState, toParams, fromState, fromParams) {
       // Analytics
+      console.log($window.ga);
       if($window._gaq && fromState.name) {
         $window._gaq.push(['_trackPageview', $location.path()]);
       }
