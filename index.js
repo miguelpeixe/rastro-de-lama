f093b8aeb501ea3f9d739b91ee5fba88f451157c
Miguel Peixe <miguel@cardume.art.br>
2017-04-03 13:36:25 -0300

    dockerize

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-var config = require('./config');
+var fs = require('fs');
+
+var env = process.env;
+var config = {
+  "fbDatabase": env.FB_DATABASE,
+  "fbToken": env.FB_TOKEN,
+  "botToken": env.BOT_TOKEN,
+  "groupId": env.GROUP_ID,
+  "fileStore": env.FILE_STORE,
+  "cloudinary": {
+    "cloud_name": env.CLOUDINARY_CLOUD_NAME,
+    "api_key": env.CLOUDINARY_API_KEY,
+    "api_secret": env.CLOUDINARY_API_SECRET
+  }
+};
+if(!config.fbDatabase && fs.existsSync('./config.json')) {
+  config = require('./config');
+}
 var Firebase = require('firebase');
 
 var bot = require('./bot');
